f5c6d820e0933156a5f7fc67a68051484fbb46f6
Hanif Fakira <fakira.work@gmail.com>
2016-06-29 11:13:04 +0100

    setup router, removed #, re route for non defined route

--- public/js/app.js
+++ public/js/app.js
@@ -2,5 +2,22 @@
 
 (function(){
   angular
-  .module("tagAnything", []);
+  .module("tagAnything", [
+    "ui.router"
+  ])
+  .config(Router);
+  Router.$inject = ["$stateProvider", "$locationProvider", "$urlRouterProvider"];
+  function Router($stateProvider, $locationProvider, $urlRouterProvider){
+    $locationProvider.html5Mode(true);
+    $stateProvider
+    .state("main", {
+      url:      "/",
+      template: "<h2>This is working!</h2>"
+    })
+    .state("test", {
+      url:      "/test",
+      templateUrl: 'test.html'
+    });
+    $urlRouterProvider.otherwise("/");
+  }
 })();
