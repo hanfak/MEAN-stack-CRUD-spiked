4b8a5ebbf95ed2cf655e6800a4651ae28e2c3e46
Hanif Fakira <fakira.work@gmail.com>
2016-06-29 13:22:46 +0100

    seed database

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,10 @@ var mongoose    = require("mongoose");
 
 var ProductSchema  = {
   name:       String
-}
+};
 
+mongoose.model("Product", ProductSchema);
 
 mongoose.connect("mongodb://localhost/taganything");
+
+module.exports = mongoose;
